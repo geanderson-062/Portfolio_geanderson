ae65e191525b8ee333b76f41f00b6c3a66c2b798
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-07-26 16:32:28 -0300

    Update Title_sobre.js

--- src/components/Title_sobre.js
+++ src/components/Title_sobre.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import "../styles/main.css";
 
 export default class Title_sobre extends Component {
   render() {
-    return <h1 className="fs-1 text-white text-center">Sobre mim</h1>;
+    return (
+      <h1 className="fs-1 text-white text-center" style={{ marginTop: 50 }}>
+        Sobre mim
+      </h1>
+    );
   }
 }
