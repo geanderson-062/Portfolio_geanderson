ef5e2fdde538a2fc68f9dec7a170aba48698afb4
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-07-26 08:59:58 -0300

    primeiro_commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+//css
+import "./styles/main.css";
+
+//bootstrapp
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+function App() {
+  return (
+    <>
+      <h1 className="corPrimaria">Olá mundo</h1>
+    </>
+  );
+}
+
+export default App;
