2bf2ca9e0797cfaae2aa2a6216a835d3465ad9ca
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-07-26 18:34:20 -0300

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <p className="fs-6 text-center corTitulo2" style={{ marginBlockEnd: 60 }}>
+        Gean-Dev ©203 , Inc
+      </p>
+    );
+  }
+}
