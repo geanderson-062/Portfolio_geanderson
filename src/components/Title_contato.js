6ae612fd57aaace074a269df046d721abab30e9a
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-07-26 16:32:20 -0300

    Update Title_contato.js

--- src/components/Title_contato.js
+++ src/components/Title_contato.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class Title_contato extends Component {
   render() {
-    return <h1 className="fs-1 text-white text-center">Contato</h1>;
+    return (
+      <h1 className="fs-1 text-white text-center" style={{ marginTop: 50 }}>
+        Contato
+      </h1>
+    );
   }
 }
