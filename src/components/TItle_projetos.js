52d38228c79f7448eb5065d8052ccbfc0b1b1c7a
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-07-26 16:32:26 -0300

    Update TItle_projetos.js

--- src/components/TItle_projetos.js
+++ src/components/TItle_projetos.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class TItle_projetos extends Component {
   render() {
-    return <h1 className="text-white fs-1 text-center">Projetos</h1>;
+    return (
+      <h1 className="text-white fs-1 text-center" style={{ marginTop: 50 }} s>
+        Projetos
+      </h1>
+    );
   }
 }
