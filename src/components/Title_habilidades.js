257dc55d71c26352c7c5f4c6ce2c41d9d942a047
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-07-26 16:32:23 -0300

    Update Title_habilidades.js

--- src/components/Title_habilidades.js
+++ src/components/Title_habilidades.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class Title_habilidades extends Component {
   render() {
-    return <h1 className="text-white fs-1 text-center">Habilidades</h1>;
+    return (
+      <h1 className="text-white fs-1 text-center" style={{ marginTop: 50 }}>
+        Habilidades
+      </h1>
+    );
   }
 }
