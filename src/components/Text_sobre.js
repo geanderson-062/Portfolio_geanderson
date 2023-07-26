8a8c8e692b7c7201b41e5401bfe3eeeb1e4fa756
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-07-26 16:31:54 -0300

    Create Text_sobre.js

---
+++ src/components/Text_sobre.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "../styles/main.css";
+
+export default class Text_sobre extends Component {
+  render() {
+    return (
+      <h6
+        className="text-center corTitulo2 fs-6"
+        style={{ marginLeft: "31%", marginRight: "31%", marginTop: 25 }}
+      >
+        Sou um desenvolvedor front-end altamente motivado e apaixonado por criar
+        experiências digitais incríveis e envolventes. Com uma formação em
+        Análise e Desenvolvimento de Sistemas pela Universidade UNINASSAU,
+        adquiri habilidades e conhecimentos essenciais para o desenvolvimento de
+        interfaces web e mobile.
+      </h6>
+    );
+  }
+}
